998b6a282a42ad9ace1a08a32d776f95ce13bb57
lazuch <lazuchs@mail.ru>
2016-06-13 23:53:07 +0300

    1-st commit

---
+++ app/src/main/java/com/lazuch/calldima/MainActivity.java
@@ -0,0 +1,29 @@
+package com.lazuch.calldima;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button bt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt = (Button) findViewById(R.id.btCallDima);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:0661412052"));
+                startActivity(intent);
+            }
+        });
+    }
+}
